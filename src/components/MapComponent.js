1f49405c16ad110826f8ff1e7a7b7da231c19aae
aladino24 <aladinozulmar@gmail.com>
2024-05-26 15:26:54 +0700

    first commit

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -16,7 +16,7 @@ L.Icon.Default.mergeOptions({
 
 const MapComponent = () => {
   const [markers, setMarkers] = useState([
-    { position: [51.505, -0.09], district: 'Kecamatan1', category: 'Kategori1', status: 'Aktif' },
+    { position: [-7.5492, 112.2333], district: 'Kecamatan1', category: 'Kategori1', status: 'Aktif' },
   ]);
   const [filteredMarkers, setFilteredMarkers] = useState(markers);
 
@@ -39,7 +39,7 @@ const MapComponent = () => {
 
   return (
     <div className="map-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <MapContainer center={[-7.5492, 112.2333]} zoom={13} style={{ height: "100vh", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
